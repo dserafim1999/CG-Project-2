1c465e92007efcfd093fc572cfabaa39976b7223
Oscar Stigzelius <oscar.stigzelius@aalto.fi>
2019-10-09 02:54:05 +0100

    Added cameras and a keypress function. The camera which should follow a ball is tested on the robothand and works

--- js/camera.js
+++ js/camera.js
@@ -1,11 +1,71 @@
-var Camera;
-
-function createCamera() {
+/*
+ * Creating a orthographic camera uses the following attributes
+ * left — Camera frustum left plane.
+ * right — Camera frustum right plane.
+ * top — Camera frustum top plane.
+ * bottom — Camera frustum bottom plane.
+ * near — Camera frustum near plane.
+ * far — Camera frustum far plane.
+ */
+function createTopOrthographicCamera() {
 	'use strict';
-	
-	Camera = new THREE.OrthographicCamera(-window.innerWidth /  8, window.innerWidth / 8, window.innerHeight / 8, -window.innerHeight /  8, 1, 1000);
-	Camera.position.x = 0;
-	Camera.position.y = 0;
-	Camera.position.z = 50;
-	Camera.lookAt(scene.position);
+	let left = -window.innerWidth / 8;
+	let right = window.innerWidth / 8;
+	let top = window.innerHeight / 8;
+	let bottom = -window.innerHeight / 8;
+	let near = 1;
+	let far = 1000;
+
+	var camera = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
+	camera.position.x = 0;
+	camera.position.y = 0;
+	camera.position.z = 50;
+	camera.lookAt(scene.position);
+
+	return camera;
+}
+
+/*
+ * Creating a perspective camera uses the following attributes
+ * fov — Camera frustum vertical field of view.
+ * aspect — Camera frustum aspect ratio.
+ * near — Camera frustum near plane.
+ * far — Camera frustum far plane.
+ */
+function createFixedPerspectiveCamera() {
+	'use strict';
+	let fov = 30;
+	let aspect = window.innerWidth / window.innerHeight;
+	let near = 1;
+	let far = 1000;
+
+	var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+	camera.position.set(100, 100, 200);
+	//camera.rotation.y += Math.PI / 2;
+	camera.lookAt(scene.position);
+
+	return camera;
+}
+
+/*
+ * Creating a perspective camera uses the following attributes
+ * fov — Camera frustum vertical field of view.
+ * aspect — Camera frustum aspect ratio.
+ * near — Camera frustum near plane.
+ * far — Camera frustum far plane.
+ */
+function createFollowBallPerspectiveCamera(ball) {
+	'use strict';
+	let fov = 45;
+	let aspect = window.innerWidth / window.innerHeight;
+	let near = 1;
+	let far = 1000;
+
+	var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+	ball.add(camera);
+
+	camera.position.set(10, 10, 0);
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+	return camera;
 }
--- js/main.js
+++ js/main.js
@@ -1,35 +1,57 @@
 var scene, renderer;
 
+var cameras = {
+	topOrthographicCamera: null,
+	fixedPerspectiveCamera: null,
+	followBallPerspectiveCamera: null
+};
+
 function onKeyDown(e) {
 	'use strict';
-	
+
 	switch (e.keyCode) {
 	}
 }
 
+function onKeyPress(e) {
+	'use strict';
+
+	switch (e.keyCode) {
+		case 49: //1
+			scene.activeCamera = cameras.topOrthographicCamera;
+			break;
+		case 50: //2
+			scene.activeCamera = cameras.fixedPerspectiveCamera;
+			break;
+		case 51: //3
+			if (cameras.followBallPerspectiveCamera !== null) {
+				scene.activeCamera = cameras.followBallPerspectiveCamera;
+			}
+			break;
+	}
+}
 
 function onKeyUp(e) {
 	'use strict';
 
-	switch(e.keyCode) {
+	switch (e.keyCode) {
 	}
 }
-
 
 function onResize() {
 	'use strict';
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	scene.activeCamera.aspect = (renderer.getSize().width / renderer.getSize().height);
+	scene.activeCamera.aspect = renderer.getSize().width / renderer.getSize().height;
 	scene.activeCamera.updateProjectionMatrix();
 }
 
 function createScene() {
 	'use strict';
-	
+
 	scene = new THREE.Scene();
-	
+
 	scene.add(new THREE.AxisHelper(10));
-	
+
 	createPlayingField(0, 0, 0);
 }
 
@@ -44,25 +66,28 @@ function animate() {
 	//canonMovement();
 
 	render();
-	
+
 	requestAnimationFrame(animate);
 }
 
 function init() {
 	'use strict';
-	
-	renderer = new THREE.WebGLRenderer( {antialias: true } );
-	
+
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	
+
 	document.body.appendChild(renderer.domElement);
-	
+
 	createScene();
-	createCamera();
-	scene.activeCamera = Camera;
+	cameras.topOrthographicCamera = createTopOrthographicCamera();
+	cameras.fixedPerspectiveCamera = createFixedPerspectiveCamera();
+	// createFollowBallPerspectiveCamera(ball) // This should only be used with a balls
+	scene.activeCamera = cameras.fixedPerspectiveCamera;
 	render();
 
-	window.addEventListener("resize", onResize);
-	window.addEventListener("keydown", onKeyDown);	
-	window.addEventListener("keyup", onKeyUp);	
+	window.addEventListener('resize', onResize);
+	window.addEventListener('keydown', onKeyDown);
+	window.addEventListener('keypress', onKeyPress);
+	window.addEventListener('keyup', onKeyUp);
 }
