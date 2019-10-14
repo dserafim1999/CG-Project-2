c217a4e62b48bd0848a26335f5d3ecb6b3d688a5
dserafim1999 <dserafim1999@gmail.com>
2019-10-14 09:47:35 +0100

    cannons now fire

--- js/ball.js
+++ js/ball.js
@@ -5,7 +5,9 @@ var geometry, material, mesh;
 function createBall(x, y, z) {
 	'use strict';
 	var ball = new THREE.Object3D();
-	material = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true });
+	var rcolor = new THREE.Color( 0xffffff );
+    rcolor.setHex( Math.random() * 0xffffff );
+	material = new THREE.MeshBasicMaterial({ color: rcolor, wireframe: true });
 	geometry = new THREE.SphereGeometry(2, 32, 32);
 	mesh = new THREE.Mesh(geometry, material);
 	ball.add(mesh);
